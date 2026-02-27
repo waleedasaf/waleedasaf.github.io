0edadb593c906d5803591071db7765d54ff23092
waleeddevops111 <devopswaleed@gmail.com>
2026-02-27 01:29:40 +0000

    Updated profile image

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
-
+import profileImg from '../assets/images/profile1.jpg';
 function Main() {
 
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={profile1Img} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
