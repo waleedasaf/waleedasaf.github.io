837adb0c057057b06c416df666bb6dd9b788f969
waleeddevops111 <devopswaleed@gmail.com>
2026-02-27 01:32:49 +0000

    Updated profile image 1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src={profile1Img} alt="Avatar" />
+          <img src={profileImg} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
